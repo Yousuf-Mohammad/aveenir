527c53526fa2abe5072ba8589e49f2c8d1a04f5e
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2024-09-26 15:05:31 +0600

    contact problem solved

--- component/Contact/Contact.js
+++ component/Contact/Contact.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React from 'react'
 import "./Contact.css";
 import 'remixicon/fonts/remixicon.css'
@@ -25,15 +25,15 @@ const Contact = () => {
             <div id='mapContainer'>
                 <iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=+(aveenir)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
                 
-                <a href="https://www.gps.ie/">gps trackers
-                </a>
+                {/* <a href="https://www.gps.ie/">gps trackers
+                </a> */}
             </iframe>
             </div>
 
             
             <h1> <i className="ri-map-pin-fill icon"> </i>84/1, Malek Tower, Road-3, Shopnodhara Housing Ltd.Bosila Road, Mohammadpur</h1>
-            <h1><Link href="mailto:aveenirIT@gmail.com"> <i className="ri-mail-send-line"></i>aveenirIT@gmail.com</Link></h1>
-            <h1><i class="ri-time-fill icon"></i>   Sunday-Thursday(10am-7pm)</h1>
+            <h1><Link href="mailto:aveenirIT@gmail.com"> <i className="ri-mail-send-line icon"></i>aveenirIT@gmail.com</Link></h1>
+            <h1><i className="ri-time-fill icon"></i>   Sunday-Thursday(10am-7pm)</h1>
             <h1><i className="ri-phone-fill icon"></i> +880 1955325537</h1>
             </div>
             
