7b9900e36ba08cd312cc5265d1b2ca3f8dcab124
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2024-10-03 10:05:30 +0600

    masonary done

--- component/FeaturedWork/FeaturedWork.js
+++ component/FeaturedWork/FeaturedWork.js
@@ -9,18 +9,23 @@ import image4 from "@/public/social (1).jpg"
 import image5 from "@/public/social (3).jpg"
 import image6 from "@/public/download (1).jpg"
 import image7 from "@/public/download.jpg"
-import image8 from "@/public/pos2.jpg"
+import image8 from "@/public/wallet.jpg"
 const FeaturedWork = () => {
   return (
     <div id='featuredWorkContainer'>
         <h1 id='FeaturedWorkHeading'>Featured Work</h1>
         <div id='featuredWork'>
         <div id= "grid1">
-               <Image src={image4} alt="image4" id="image4" />
-               <Image src={image5} alt="image5" id="image5" />
-               <Image src={image6} alt="image6" id="image6" />
-               <Image src={image7} alt="image7" id="image7" />
-               <Image src={image8} alt="image8" id="image8" />
+              <div id="imageContainer1"><Image src={image4} alt="image" id="image" /></div>
+              <div id="imageContainer2"><Image src={image7} alt="image5" id="image" /></div>
+              <div id="imageContainer3"><Image src={image5} alt="image6" id="image" /></div>
+              <div id="imageContainer4"><Image src={image8} alt="image7" id="image7" /></div>
+              <div id="imageContainer5"><Image src={image6} style={{height:"100%"}} alt="image8" id="image8" /></div>
+               {/* 
+               
+               
+               
+                */}
         </div>
         <div id= "grid2">
                <Image src={image1} alt="image1" id="image1" />
