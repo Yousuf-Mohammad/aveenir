fb834b3f2bef8a116b7656a86f3227b6274b5197
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2024-11-13 16:05:30 +0600

    autoprefixer added

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ['tailwindcss', 'autoprefixer'],
-
+ 'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
 };
 
 export default config;
